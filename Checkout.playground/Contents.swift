98be49c1f8b94e01d8867042ac9e9e38d6b26806
Thomas <Roff>
2024-01-27 15:21:48 +1300

    Add failing test

--- Checkout.playground/Contents.swift
+++ Checkout.playground/Contents.swift
@@ -4,16 +4,25 @@ import XCTest
 class Checkout {
     
     let total = 0
+    
+    func scan(_ product: String) {
+    }
 
 }
 
 class CheckoutTests: XCTestCase {
     
     let co: Checkout = Checkout()
-   
+
     func testEmpty() {
         XCTAssertEqual(0, co.total)
     }
+    
+    func testOne() {
+        co.scan("A")
+        XCTAssertEqual(50, co.total)
+    }
+    
 }
 
 CheckoutTests.defaultTestSuite.run()
